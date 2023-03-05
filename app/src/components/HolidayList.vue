3b677061d45e5215ca9f96d6cffd375272f64f21
Jonathan Sinclair <jonathan-sinclair1@live.co.uk>
2023-03-05 18:46:29 +0000

    formatted date strings for front end tables

--- app/src/components/HolidayList.vue
+++ app/src/components/HolidayList.vue
@@ -1,42 +1,33 @@
 <template>
-  <ion-list>
-    <ion-list-header>{{ store.getBankHolidays.display_name }}</ion-list-header>
-    
-    <ion-grid>
-      <ion-row v-for="day in store.getBankHolidays.holidays" :key="day">
-        <ion-col>
-          <ion-title>
+  <ion-page>
+    <ion-list>
+      <ion-list-header>{{ store.getBankHolidays.display_name }}</ion-list-header>
+      
+      <ion-grid :fixed="true" class="ion-text-center">
+        <ion-row v-for="day in store.getBankHolidays.holidays" :key="day">
+          <ion-col class="ion-text-wrap">
             <ion-text>
-              {{ day.name }}
+              {{ getDateShortFormatString(day.date) }}
             </ion-text>
-          </ion-title>
-        </ion-col>
-        <ion-col>
-          <ion-title>
-            <ion-text>
-              {{ day.date }}
-            </ion-text>
-          </ion-title>
-        </ion-col>
-        <ion-col>
-          <ion-title>
-            <ion-text>
-              Day Of The Week
-            </ion-text>
-          </ion-title>
-        </ion-col>
+          </ion-col>
+          <ion-col>
+            {{ getDayOfWeekString(day.date) }}
+          </ion-col>
+          <ion-col class="ion-text-wrap">
+            {{ day.name }}
+          </ion-col>
+          <ion-col>
+            {{ getYearNumber(day.date) }}
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </ion-list>
 
-        <!-- <ion-item>
-          <ion-label>{{ day.name }} </ion-label>
-          <ion-label> {{ day.date }}</ion-label> 
-          <ion-label>{{ day.name }} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {{ day.date }} </ion-label>
-        </ion-item> -->
-      </ion-row>
-    </ion-grid>
-  </ion-list>
+  </ion-page>
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from "vue";
 import { useCountryStore } from "@/stores/CountryStore";
 import { IonGrid, IonCol, IonText } from "@ionic/vue";
@@ -50,6 +41,123 @@ export default defineComponent({
     IonGrid,
     IonCol,
     IonText
+  },
+  methods: {
+    getDayOfWeekString(date: string): string {
+      const day = new Date(date).getDay();
+
+      let dayOfWeek = "";
+      switch(day) {
+        case 0: {
+          dayOfWeek = "Sunday";
+          break; 
+        }
+        case 1: {
+          dayOfWeek = "Monday";
+          break;
+        }
+        case 2: {
+          dayOfWeek = "Tuesday";
+          break;
+        }
+        case 3: {
+          dayOfWeek = "Wednesday";
+          break;
+        }
+        case 4: {
+          dayOfWeek = "Thursday";
+          break;
+        }
+        case 5: {
+          dayOfWeek = "Friday";
+          break;
+        }
+        case 6: {
+          dayOfWeek = "Saturday";
+          break;
+        }
+      }
+      return dayOfWeek;
+    },
+    getDateShortFormatString(date: string): string {
+      const dayNumber = new Date(date).getDate();
+      const monthNumber = new Date(date).getMonth();
+      // const yearNumber = new Date(date).getFullYear();
+
+      let monthName = "";
+      switch(monthNumber) {
+        case 0: {
+          monthName = "January";
+          break; 
+        }
+        case 1: {
+          monthName = "Februrary";
+          break;
+        }
+        case 2: {
+          monthName = "March";
+          break;
+        }
+        case 3: {
+          monthName = "April";
+          break;
+        }
+        case 4: {
+          monthName = "May";
+          break;
+        }
+        case 5: {
+          monthName = "June";
+          break;
+        }
+        case 6: {
+          monthName = "July";
+          break;
+        }
+        case 7: {
+          monthName = "August";
+          break;
+        }
+        case 8: {
+          monthName = "September";
+          break;
+        }
+        case 9: {
+          monthName = "October";
+          break;
+        }
+        case 10: {
+          monthName = "November";
+          break;
+        }
+        case 11: {
+          monthName = "December";
+          break;
+        }
+      }
+
+      // const dateShortFormat = `${dayNumber} ${monthName} ${yearNumber}`
+      const dateShortFormat = `${dayNumber} ${monthName}`
+
+      return dateShortFormat;
+    }
+    ,getYearNumber(date: string): string {
+      const yearNumber = new Date(date).getFullYear()
+
+      const yearNumberAsString = `${yearNumber}`
+
+      return yearNumberAsString;
+    }
   }
 });
 </script>
+
+
+<style scoped>
+  ion-col {
+    line-height: 1.25;
+    width: 100%;
+    margin-bottom: 20px;
+    border-spacing: 0;
+  }
+</style>
