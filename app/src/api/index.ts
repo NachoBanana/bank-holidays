9307a8dbe8eaf16369cd6964466541af855c36fd
VL <44089845+Vitali-Lupusor@users.noreply.github.com>
2023-04-09 15:05:12 +0100

    chore: debugging

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -3,6 +3,6 @@ export async function http(url: string) {
     return await response.json();
 }
 
-const HOST_API = process.env.VUE_APP_HOST_API || "localhost";
+const HOST_API = process.env.VUE_APP_HOST_API || "http://localhost";
 const PORT_API = process.env.VUE_APP_PORT_API || 8080;
-export const API_ADDRESS = `http://${HOST_API}:${PORT_API}/v1`;
+export const API_ADDRESS = `${HOST_API}:${PORT_API}/v1`;
